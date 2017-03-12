ff05a25ab02fe383743bba94510d8322c545fadc
MeanBU <unmika.watt@bumail.net>
2017-03-12 13:18:36 +0700

    step 3 Ok

--- app/src/main/java/mean/chan/mind/sendgps/SplashScreen.java
+++ app/src/main/java/mean/chan/mind/sendgps/SplashScreen.java
@@ -1,5 +1,8 @@
 package mean.chan.mind.sendgps;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,22 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-    }
-}
+
+        //Sound Effect
+        MediaPlayer mediaPlayer = MediaPlayer.create(SplashScreen.this,
+                R.raw.intro_tata);
+        mediaPlayer.start();
+
+        //Post Delay & Intent to HomeActivity
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, HomeActivity.class));
+                finish();
+            }
+        }, 9000);
+
+    }   // Main Method
+
+}   // Main Class
